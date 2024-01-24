29acf4221babda2f6934e9bb058bc4a691db1c1f
akademicu <akademicu@mail.ru>
2024-01-24 10:31:41 +0000

    First commit

---
+++ day19.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def turn_clockwise():
+    tim.right(5)
+
+def turn_counter_clockwise():
+    tim.left(5)
+
+
+screen.listen()
+screen.onkey("w", fun = move_forwards)
+screen.onkey(key='s',fun= move_backwards)
+screen.onkey(key='d', fun=turn_clockwise)
+screen.onkey(key='a', fun=turn_counter_clockwise)
+screen.exitonclick()
