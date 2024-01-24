8525af082831f77a32cd00b038fa0266b85300b2
akademicu <akademicu@mail.ru>
2024-01-24 10:43:02 +0000

    key change

--- day19.py
+++ day19.py
@@ -16,7 +16,7 @@ def turn_counter_clockwise():
 
 
 screen.listen()
-screen.onkey("w", fun = move_forwards)
+screen.onkey(key = "w", fun = move_forwards)
 screen.onkey(key='s',fun= move_backwards)
 screen.onkey(key='d', fun=turn_clockwise)
 screen.onkey(key='a', fun=turn_counter_clockwise)
